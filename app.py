0d3892329587b01b7b7eda656008deda6ddc2234
jiangyang <jiangyangcreate@gmail.com>
2024-10-20 12:28:34 +0800

    📝

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def media_interface():
     return render_template('media_interface.html', screen=screen, camera=camera, sound=sound)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=8080)
